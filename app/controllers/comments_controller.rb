97c86f8d3a27b4f3a464094e351d43d3284b7863
Caleb Rodriguez <sirdigo6@gmail.com>
2022-02-14 19:41:52 -0500

    Worked on Comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,43 @@
 class CommentsController < ApplicationController
+    before_action :error_message_not_found, unless: :find_by_id, only: [:destroy, :update]
 
     #GET /comments
 
 
     #POST /comments
-
+    def create
+        comment = Comment.new(params_permit)
+        if comment.save
+            render json: comment, status: :created
+        else
+            render json: { errors: [comment.errors.full_messages] }, status: :unprocessable_entity
+        end
+    end
 
     #DELETE /comments/:id
-
+    def destroy
+        @comment.destroy
+        head :no_content
+    end
 
     #PATCH /comments/:id
+    def update
+        @comment.update(params_permit)
+        render json: @comment, status: :ok
+    end
+
+    #Private Methods
+    private
+
+    def find_by_id
+        @comment = Comment.find_by(id: params[:id])
+    end
+
+    def params_permit
+        params.permit(:content, :post_id, :user_id, :commentor)
+    end
+
+    def error_message_not_found
+        render json: { error: "Comment not found" }, status: :not_found
+    end
 end
