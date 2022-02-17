1a0a4d73cfd5978c6ca570c09c74dd0f4eacab70
Defne Celikoyar <defne.celikoyar@gmail.com>
2022-02-16 20:48:36 -0500

    yes.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,14 @@ Comment.destroy_all
 
 puts "Seeding users..."
 
-5.times{User.create({username: Faker::Internet.username, password_digest: Faker::Internet.password, email: Faker::Internet.email})}
+25.times{User.create({username: Faker::Internet.username, password_digest: Faker::Internet.password, email: Faker::Internet.email})}
 
 puts "Seeding Posts..."
 
-10.times{Post.create({title: Faker::Movie.title, content: Faker::Lorem.paragraph, user_id: User.all.sample.id})}
+50.times{Post.create({title: Faker::Movie.title, content: Faker::Lorem.paragraph, user_id: User.all.sample.id})}
 
 puts "Seeding comments..."
 
-50.times{Comment.create({content: Faker::Lorem.sentences, post_id: Post.all.sample.id, user_id: User.all.sample.id})}
+100.times{Comment.create({content: Faker::Lorem.sentences, post_id: Post.all.sample.id, user_id: User.all.sample.id})}
 
 puts "Done Seeding!"
