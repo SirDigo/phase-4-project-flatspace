bf34152cd31aeaefa985782a06a2bb925f3d6810
Defne Celikoyar <defne.celikoyar@gmail.com>
2022-02-17 18:40:16 -0500

    stuff

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,45 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
-function Header( {state} ) {
-  return (
-    <div style={{backgroundColor: "#CFDECF"}}>
-      <div className="container">
-        <div className="row">
-          <h2 className="six columns u-pull-left" style={{color: "#3A553A"}}>Flatspace</h2>
-          <br></br>
-          <h5 className="two columns"><Link to="/about">About</Link></h5>
-          <h5 className="two columns"><Link to="/login">Login</Link></h5>
-          <h5 className="two columns"><Link to="/signup">SignUp</Link></h5>
+function Header( {isAuthenticated, profile, setIsAuthenticated, setUser} ) {
+  const navigate = useNavigate();
+  console.log(isAuthenticated);
+
+  const logout = () => {
+    fetch('/logout',{
+        method:'DELETE'
+    })
+    .then(()=>{
+        setIsAuthenticated(false);
+        setUser(null);
+        navigate("/")
+    })}
+
+    return (
+      <div style={{backgroundColor: "#CFDECF"}}>
+        <div className="container">
+          <div className="row">
+            <h2 className="six columns u-pull-left" style={{color: "#3A553A"}}><a href="/">Flatspace</a></h2>
+            <br></br>
+            {isAuthenticated ? (
+            <>
+              <h5 className="two columns"><Link to="/about">about</Link></h5>
+              <h5 className="two columns"><a href={`/${profile.id}`}>profile</a></h5>
+              <h5 className="two columns"><Link to="/" onClick={logout}>logout</Link></h5>
+              <h5 className="two columns"><Link to="/addpost">post</Link></h5>
+            </>
+            ) : (
+            <>
+              <h5 className="two columns"><Link to="/about">about</Link></h5>
+              <h5 className="two columns"><Link to="/login">login</Link></h5>
+              <h5 className="two columns"><Link to="/signup">sign up</Link></h5>
+            </>
+            )
+            }
+          </div>
         </div>
       </div>
-    </div>
-  );
+    )
 }
 
 export default Header;
